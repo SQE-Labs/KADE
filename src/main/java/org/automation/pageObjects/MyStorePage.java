9c5ce8c626eb1a9d7a1c66fb1984e103d19ee03f
Shruti <shruti.saroha@sqelabs.com>
2024-07-25 17:48:16 +0530

    Added 3 Test Cases

--- src/main/java/org/automation/pageObjects/MyStorePage.java
+++ src/main/java/org/automation/pageObjects/MyStorePage.java
@@ -1,5 +1,7 @@
 package org.automation.pageObjects;
 
+import java.awt.*;
+import java.nio.file.Paths;
 import java.util.List;
 
 import org.automation.base.BasePage;
@@ -13,11 +15,31 @@ public class MyStorePage extends BasePage {
 	By skipStripeAccountPopUpBtn=By.xpath("//div[@class='modal-content']//button[text()='Skip']");
 	By deleteStoreBtn=By.xpath("//button[text()='Delete the store']");
 	By deleteStoreIcon = By.cssSelector(".fa.fa-check");
+	By storeLogo=By.xpath("(//img[contains(@alt,'')])[3]");
+	By businessNameTbx=By.xpath("//input[@name='name']");
+	By storeAddressField=By.cssSelector(".form-control.pac-target-input");
+	By storeAddressOption=By.xpath("(//div[@class='pac-item'])[1]");
+	By phoneTbx=By.xpath("//input[@name='phone']");
+	By timeZoneField=By.xpath("//select[@name='timeZone']");
+	By timeZoneOption=By.xpath("//option[text()='(GMT-05:00) Eastern Time (US & Canada)']");
+	By taxRateTbx=By.xpath("//input[@name='taxRate']");
+	By saveBtn=By.xpath("//button[text()='Save']");
+	By stripeBtn=By.cssSelector(".img-fluid.h-100");
+	By testStripeBtn=By.partialLinkText("Create a test Stripe account");
+	By bankTransferToggleBtn=By.xpath("(//i[@class='far fa-toggle-on fa-rotate-180 custom-check-off '])[3]");
+	By configureCreditCardTerminal=By.xpath("//button[@class='btn btn-link']");
+	By creditCardTerminalOption=By.xpath("//span[text()=' Tap-To-Pay on phone']");
+	By creditSaveBtn=By.xpath("//button[text()='Save']");
+	By skipForNowBtn=By.xpath("//button[text()='Skip for now']");
+	By continueBtn=By.xpath("//button[text()='Continue']");
+	By configureLink=By.xpath("//div[contains(@class,'row')]//div[3]//div[1]//a[1]");
+	By modifyBtn=By.xpath("//button[text()='Modify']");
 
 	public void clickRegisterNewBusinessBtn() {
 		click(registerNewBusinessBtn);
 	}
 	public void clickSkipStripeAccountBtn() {
+		WebdriverWaits.waitForElementClickable(skipStripeAccountBtn, 5);
 		click(skipStripeAccountBtn);
 	}
 	public void clickSkipStripeAccountPopUpBtn() {
@@ -32,5 +54,77 @@ public class MyStorePage extends BasePage {
 		WebdriverWaits.waitForElementUntilVisible(deleteStoreIcon, 5);
 		clickElementByJS(deleteStoreIcon);
 	}
-
+	public void clickStoreLogo(){
+		clickElementByJS(storeLogo);
+	}
+	public void uploadImageAsAttachment(String relativePath) throws AWTException {
+		String projectPath = System.getProperty("user.dir");
+		String absolutePath = Paths.get(projectPath, relativePath).toString();
+		uploadImageFile(absolutePath);
+	}
+	public void enterBusinessName(String businessName) {
+		WebdriverWaits.waitForElementUntilVisible(businessNameTbx, 2);
+		clear_custom(businessNameTbx);
+		pressKeys(businessNameTbx, businessName);
+		click(businessNameTbx);
+	}
+	public void selectStoreAddress(String storeAddressName){
+		WebdriverWaits.waitForElementUntilVisible(storeAddressField, 2);
+		clear_custom(storeAddressField);
+		pressKeys(storeAddressField, storeAddressName);
+		click(storeAddressField);
+		click(storeAddressOption);
+	}
+	public void enterPhone(String phone){
+		WebdriverWaits.waitForElementUntilVisible(phoneTbx, 2);
+		clear_custom(phoneTbx);
+		pressKeys(phoneTbx, phone);
+		click(phoneTbx);
+	}
+	public void selectTimeZone(){
+		click(timeZoneField);
+		click(timeZoneOption);
+	}
+	public void enterTax(String taxRate){
+		WebdriverWaits.waitForElementUntilVisible(taxRateTbx, 2);
+		clear_custom(taxRateTbx);
+		pressKeys(taxRateTbx, taxRate);
+		click(taxRateTbx);
+	}
+	public void clickSaveBtn(){
+		WebdriverWaits.waitForElementUntilVisible(saveBtn, 5);
+		moveToWebElement(saveBtn);
+		clickElementByJS(saveBtn);
+	}
+	public void clickStripeBtn(){
+		click(stripeBtn);
+	}
+	public void clickTestStripeBtn(){
+		click(testStripeBtn);
+	}
+	public void enableBankTransfer(){
+		click(bankTransferToggleBtn);
+	}
+	public void clickConfigureCreditCardTerminals(){
+		click(configureCreditCardTerminal);
+	}
+	public void selectCreditCardTerminal(){
+		click(creditCardTerminalOption);
+	}
+	public void clickCreditSaveBtn(){
+		click(creditSaveBtn);
+	}
+	public void clickSkipForNowBtn(){
+		click(skipForNowBtn);
+	}
+	public void clickContinueBtn(){
+		moveToWebElement(continueBtn);
+		click(continueBtn);
+	}
+	public void clickConfigureLink(){
+		click(configureLink);
+	}
+	public void clickModifyBtn(){
+		click(modifyBtn);
+	}
 }
--- src/test/java/MyStoreTest.java
+++ src/test/java/MyStoreTest.java
@@ -1,3 +1,4 @@
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -29,21 +30,90 @@ public class MyStoreTest extends BaseTest {
 	public void logout() {
 		dashboard.signOut();
 	}
-
-	@Test(enabled = true, description = "Verifying deletion of Store when Stripe Account is not Configured")
-	public void tc01_DeletionOfStore() {
+	@Test(description="TC_01 Verifying creation of Store without Stripe Payment Account Configuration")
+	public void tc01_StoreCreationWithoutStripeAccount() throws AWTException {
+		//Clicking MyStore sub-tab
 		dashboard.clickOnMyStores();
 
+		//Creating New Store
 		myStore.clickRegisterNewBusinessBtn();
 		myStore.clickSkipStripeAccountBtn();
 		WebdriverWaits.sleep(3000);
 		myStore.clickSkipStripeAccountPopUpBtn();
+		WebdriverWaits.sleep(3000);
+
+		//Entering Store Details
+		myStore.clickStoreLogo();//Fix this
+		myStore.uploadImageAsAttachment("src/main/resources/image/BillDummyImg.jpg");
+		String businessName = "Test Store-1";
+		myStore.enterBusinessName(businessName);
+		String storeAddressName="123";
+		myStore.selectStoreAddress(storeAddressName);
+		String phone="9180652348";
+		myStore.enterPhone(phone);
+		myStore.selectTimeZone();
+		String taxRate="10.000";
+		myStore.enterTax(taxRate);
+		myStore.clickSaveBtn();//Fix this
+	}
+
+	@Test(description = "TC_02 Verifying deletion of Store when Stripe Account is not Configured")
+	public void tc02_DeletionOfStore() {
+		//Clicking MyStore sub-tab
+		dashboard.clickOnMyStores();
+
+		myStore.clickRegisterNewBusinessBtn();
+
+		//Skipping Stripe Account option
+		myStore.clickSkipStripeAccountBtn();
+		WebdriverWaits.sleep(3000);
+		myStore.clickSkipStripeAccountPopUpBtn();
 		WebdriverWaits.sleep(5000);
+
+		//Deleting the Store
 		myStore.clickDeleteStoreBtn();
 		WebdriverWaits.sleep(3000);
 		myStore.clickDeleteStoreIcon();
+	}
+	@Test(description = "TC_03 Verify creation of Store with Stripe Payment Account including terminal configuration")
+	public void tc03_CreationOfStoreWithStripeAccount() {
+		//Clicking MyStore sub-tab
+		dashboard.clickOnMyStores();
+
+		myStore.clickRegisterNewBusinessBtn();
+		myStore.clickStripeBtn();
+
+		//Creating test Stripe Account
+		myStore.clickTestStripeBtn();
+		myStore.enableBankTransfer();
+		myStore.clickContinueBtn();
+		myStore.clickConfigureCreditCardTerminals();
+		myStore.selectCreditCardTerminal();
+		myStore.clickCreditSaveBtn();
+		myStore.clickSkipForNowBtn();
+		myStore.clickContinueBtn();
+	}
+
+	@Test(description = "TC_04 Verifying modification of existing created Store")
+	public void tc04_VerifyingModificationOfExistingCreatedStore() {
+		//Clicking MyStore sub-tab
+		dashboard.clickOnMyStores();
+
+		myStore.clickConfigureLink();
+		myStore.clickModifyBtn();
+		String businessName = "Test Store-2";
+		myStore.enterBusinessName(businessName);
+		String storeAddressName="123";
+		myStore.selectStoreAddress(storeAddressName);
+		String phone="9180652341";
+		myStore.enterPhone(phone);
+		myStore.selectTimeZone();
+		String taxRate="18.000";
+		myStore.enterTax(taxRate);
+		myStore.clickSaveBtn();//Fix this
 
 	}
+
 }
 
 
