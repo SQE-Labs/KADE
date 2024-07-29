ebb1432f5b657b83242932354bcb5d366db58f1d
Shruti <shruti.saroha@sqelabs.com>
2024-07-29 18:31:22 +0530

    Added 1 Test Case

--- src/main/java/org/automation/pageObjects/MyStorePage.java
+++ src/main/java/org/automation/pageObjects/MyStorePage.java
@@ -10,30 +10,69 @@ import org.openqa.selenium.By;
 
 public class MyStorePage extends BasePage {
 
-	By registerNewBusinessBtn=By.partialLinkText("Register new business");
-	By skipStripeAccountBtn=By.cssSelector(".btn-lg.fw-bold.w-100.btn.btn-outline-primary");
-	By skipStripeAccountPopUpBtn=By.xpath("//div[@class='modal-content']//button[text()='Skip']");
-	By deleteStoreBtn=By.xpath("//button[text()='Delete the store']");
+	By registerNewBusinessBtn = By.partialLinkText("Register new business");
+	By skipPopUpTitle=By.xpath("//h5[text()='Skip']");
+	By skipStripeAccountBtn = By.cssSelector(".btn-lg.fw-bold.w-100.btn.btn-outline-primary");
+	By skipStripeAccountPopUpBtn = By.xpath("//div[@class='modal-content']//button[text()='Skip']");
+	By deleteStoreBtn = By.xpath("//button[text()='Delete the store']");
 	By deleteStoreIcon = By.cssSelector(".fa.fa-check");
-	By storeLogo=By.xpath("(//img[contains(@alt,'')])[3]");
-	By businessNameTbx=By.xpath("//input[@name='name']");
-	By storeAddressField=By.cssSelector(".form-control.pac-target-input");
-	By storeAddressOption=By.xpath("(//div[@class='pac-item'])[1]");
-	By phoneTbx=By.xpath("//input[@name='phone']");
-	By timeZoneField=By.xpath("//select[@name='timeZone']");
-	By timeZoneOption=By.xpath("//option[text()='(GMT-05:00) Eastern Time (US & Canada)']");
-	By taxRateTbx=By.xpath("//input[@name='taxRate']");
-	By saveBtn=By.xpath("//button[text()='Save']");
-	By stripeBtn=By.cssSelector(".img-fluid.h-100");
-	By testStripeBtn=By.partialLinkText("Create a test Stripe account");
-	By bankTransferToggleBtn=By.xpath("(//i[@class='far fa-toggle-on fa-rotate-180 custom-check-off '])[3]");
-	By configureCreditCardTerminal=By.xpath("//button[@class='btn btn-link']");
-	By creditCardTerminalOption=By.xpath("//span[text()=' Tap-To-Pay on phone']");
-	By creditSaveBtn=By.xpath("//button[text()='Save']");
-	By skipForNowBtn=By.xpath("//button[text()='Skip for now']");
-	By continueBtn=By.xpath("//button[text()='Continue']");
-	By configureLink=By.xpath("//div[contains(@class,'row')]//div[3]//div[1]//a[1]");
-	By modifyBtn=By.xpath("//button[text()='Modify']");
+	By blankFieldWarningMsg=By.xpath("//p[@class='alert-content']");
+	By storeLogo = By.xpath("(//img[contains(@alt,'')])[3]");
+	By businessNameTbx = By.xpath("//input[@name='name']");
+	By locationDescTbx=By.xpath("//input[@name='description']");
+	By storeAddressField = By.cssSelector(".form-control.pac-target-input");
+	By storeAddressOption = By.xpath("(//div[@class='pac-item'])[1]");
+	By phoneTbx = By.xpath("//input[@name='phone']");
+	By timeZoneField = By.xpath("//select[@name='timeZone']");
+	By timeZoneOption = By.xpath("//option[text()='(GMT-05:00) Eastern Time (US & Canada)']");
+	By taxRateTbx = By.xpath("//input[@name='taxRate']");
+	By saveBtn = By.xpath("//button[text()='Save']");
+	By stripeBtn = By.cssSelector(".img-fluid.h-100");
+	By connectStripePopUpTitle=By.xpath("//h5[text()='Connect to stripe']");
+	By testStripeBtn = By.partialLinkText("Create a test Stripe account");
+	By stripeAccSuccessMsg=By.xpath("(//div[@class='alert-message'])");
+	By bankTransferToggleBtn = By.xpath("//span[text()='Accept bank transfer']");
+	By configureCreditCardTerminal = By.xpath("//button[@class='btn btn-link']");
+	By creditCardTerminalOption = By.xpath("//span[text()=' Tap-To-Pay on phone']");
+	By creditSaveBtn = By.xpath("//button[text()='Save']");
+	By skipForNowBtn = By.xpath("//button[text()='Skip for now']");
+	By continueBtn = By.xpath("//button[text()='Continue']");
+	By configureLink = By.xpath("(//a[contains(@class,'float-end btn btn-link')][text()='Configure'])[3]");
+	By modifyBtn = By.xpath("//button[text()='Modify']");
+	By plansSubTab = By.partialLinkText("Plans");
+	By currentPlanMSg=By.xpath("//span[@class='text-success me-1']");
+	By planSignUpBtn = By.partialLinkText("Sign up");
+	By yearlyBtn = By.xpath("//label[text()='Yearly']");
+	By termsCbx = By.xpath("//span[text()='     I agree to the']");
+	By changePlanBtn = By.xpath("//button[text()='Change plan']");
+	By addedStoreName=By.cssSelector("div[class='form-group mb-3'] h4");
+	By addedBusinessName=By.cssSelector("div[class='display-none -readonly-div-'] span");
+	By addedStoreAddress=By.xpath("//label[text()='Store Address']/following-sibling::p");
+	By addedStorePhone=By.xpath("//label[text()='Store Phone']/following-sibling::p");
+	By addedCurrencyOfStore=By.xpath("//label[text()='Currency of the Store']/following-sibling::p");
+	By addedTaxRate=By.xpath("//label[text()='Tax rate']/following-sibling::p");
+	By addedVisaMethod=By.cssSelector("div[class='-title- d-flex flex-column'] span[class='text-nowrap']");
+	By newCreditCardBtn=By.xpath("//span[text()='New Credit Card']");
+	By newBankAccountBtn=By.xpath("//span[text()='New Bank Account']");
+	By changePayMethodBtn=By.xpath("//button[text()='change']");
+	By nextBillDate=By.xpath("(//div[@class='d-flex'])[2]");
+	By deafaultVisaMethod=By.xpath("//div[contains(@class,'d-flex justify-content-center align-items-center')]");
+	By configureBtnWithoutStripe=By.xpath("(//a[contains(@class,'float-end btn btn-link')][text()='Configure'])[4]");
+	By settingsSubTab=By.xpath("//a[text()='Settings']");
+	By maxBillAmountTbx=By.xpath("//input[@name='maxBillAmountThreshold']");
+	By tipGratuityToggleBtn=By.xpath("//span[@class='ms-2 custom-check-on'][text()='No']");
+	By tipConfigureBtn=By.xpath("//div[@class='checked-d-none']//button[@type='button'][text()='Configure']");
+	By tipConfgPopUpTitle=By.xpath("//h5[text()='Tip configuration']");
+	By tipPercentField1=By.xpath("//input[@name='tipSelections[0].percentage']");
+	By tipPercentField2=By.xpath("//input[@name='tipSelections[1].percentage']");
+	By tipPercentField3=By.xpath("//input[@name='tipSelections[2].percentage']");
+	By saveChangesBtn=By.xpath("//button[text()='Save changes']");
+	By rewardConfigureBtn=By.xpath("(//button[@type='button'][text()='Configure'])[2]");
+	By rewardConfigPopUpTitle=By.xpath("//h5[text()='Rewards Program Configuration']");
+	By rewardPointToggleBtn=By.xpath("//span[@class='ms-2 fs-6 custom-check-off']");
+	By rewardPointsField=By.xpath("//input[@name='pointsForGiftcard']");
+	By rewardPtsValue=By.xpath("//input[@name='ponitsValue']");
+
 
 	public void clickRegisterNewBusinessBtn() {
 		click(registerNewBusinessBtn);
@@ -41,6 +80,9 @@ public class MyStorePage extends BasePage {
 	public void clickSkipStripeAccountBtn() {
 		WebdriverWaits.waitForElementClickable(skipStripeAccountBtn, 5);
 		click(skipStripeAccountBtn);
+	}
+	public String getSkipPopUpTitle(){
+		return getText_custom(skipPopUpTitle);
 	}
 	public void clickSkipStripeAccountPopUpBtn() {
 		WebdriverWaits.waitForElementClickable(skipStripeAccountPopUpBtn, 5);
@@ -50,11 +92,15 @@ public class MyStorePage extends BasePage {
 		WebdriverWaits.waitForElementUntilVisible(deleteStoreBtn, 5);
 		clickElementByJS(deleteStoreBtn);
 	}
+	public String getBlankFieldWarningMsg(){
+		return getText_custom(blankFieldWarningMsg);
+	}
 	public void clickDeleteStoreIcon(){
 		WebdriverWaits.waitForElementUntilVisible(deleteStoreIcon, 5);
 		clickElementByJS(deleteStoreIcon);
 	}
 	public void clickStoreLogo(){
+		moveToWebElement(storeLogo);
 		clickElementByJS(storeLogo);
 	}
 	public void uploadImageAsAttachment(String relativePath) throws AWTException {
@@ -67,6 +113,15 @@ public class MyStorePage extends BasePage {
 		clear_custom(businessNameTbx);
 		pressKeys(businessNameTbx, businessName);
 		click(businessNameTbx);
+	}
+	public void enterLocationDescription(String locDescription) {
+		WebdriverWaits.waitForElementUntilVisible(locationDescTbx, 2);
+		clear_custom(locationDescTbx);
+		pressKeys(locationDescTbx, locDescription);
+		click(locationDescTbx);
+	}
+	public String getBusinessFieldMaxLen(){
+		return getAttribute(businessNameTbx,"maxlength");
 	}
 	public void selectStoreAddress(String storeAddressName){
 		WebdriverWaits.waitForElementUntilVisible(storeAddressField, 2);
@@ -81,6 +136,9 @@ public class MyStorePage extends BasePage {
 		pressKeys(phoneTbx, phone);
 		click(phoneTbx);
 	}
+	public String getPhoneFieldMaxLen(){
+		return getAttribute(phoneTbx,"maxlength");
+	}
 	public void selectTimeZone(){
 		click(timeZoneField);
 		click(timeZoneOption);
@@ -91,6 +149,15 @@ public class MyStorePage extends BasePage {
 		pressKeys(taxRateTbx, taxRate);
 		click(taxRateTbx);
 	}
+	public String getTaxFieldMinValue(){
+		return getAttribute(taxRateTbx,"min");
+	}
+	public String getTaxFieldDefaultValue(){
+		return getAttribute(taxRateTbx,"value");
+	}
+	public String getTaxFieldMaxValue(){
+		return getAttribute(taxRateTbx,"max");
+	}
 	public void clickSaveBtn(){
 		WebdriverWaits.waitForElementUntilVisible(saveBtn, 5);
 		moveToWebElement(saveBtn);
@@ -99,8 +166,14 @@ public class MyStorePage extends BasePage {
 	public void clickStripeBtn(){
 		click(stripeBtn);
 	}
+	public String getConnectStripePopUpTitle(){
+		return getText_custom(connectStripePopUpTitle);
+	}
 	public void clickTestStripeBtn(){
 		click(testStripeBtn);
+	}
+	public String getStripeAccountSuccessMsg(){
+		return getText_custom(stripeAccSuccessMsg);
 	}
 	public void enableBankTransfer(){
 		click(bankTransferToggleBtn);
@@ -125,6 +198,141 @@ public class MyStorePage extends BasePage {
 		click(configureLink);
 	}
 	public void clickModifyBtn(){
+		moveToWebElement(modifyBtn);
 		click(modifyBtn);
+	}
+	public String getAddedStoreName(){
+		return getText_custom(addedStoreName);
+	}
+	public String getAddedBusinessName(){
+		return getText_custom(addedBusinessName);
+	}
+	public String getAddedStoreAddress(){
+		return getText_custom(addedStoreAddress);
+	}
+	public String getAddedStorePhone(){
+		return getText_custom(addedStorePhone);
+	}
+	public String getAddedCurrencyOfStore(){
+		return getText_custom(addedCurrencyOfStore);
+	}
+	public String getAddedTaxRate(){
+		return getText_custom(addedTaxRate);
+	}
+	public void clickPlansSubTab() {
+		click(plansSubTab);
+	}
+	public void clickPlanSignUpBtn() {
+		moveToWebElement(planSignUpBtn);
+		click(planSignUpBtn);
+	}
+	public void selectTermsCbx() {
+		click(termsCbx);
+	}
+	public void clickChangePlanBtn() {
+		click(changePlanBtn);
+	}
+	public void clickYearlyBtn() {
+		click(yearlyBtn);
+	}
+	public String getCurrentPlanSuccessMSg(){
+		return getText_custom(currentPlanMSg);
+	}
+	public String defaultPaymentMethod(){
+		return getText_custom(addedVisaMethod);
+	}
+	public void selectVisaMethod(){
+		click(deafaultVisaMethod);
+	}
+	public boolean isNewCreditCardBtnDisplayed(){
+		return isWebElementVisible(newCreditCardBtn);
+	}
+	public boolean isNewBankAccountBtnDisplayed(){
+		return isWebElementVisible(newBankAccountBtn);
+	}
+	public void clickChangePayMethodBtn(){
+		click(changePayMethodBtn);
+	}
+	public boolean isNextBillDateDisplayed(){
+		return isWebElementVisible(nextBillDate);
+	}
+	public void clickConfigureBtn(){
+		click(configureBtnWithoutStripe);
+	}
+	public void clickSettingsSubTab(){
+		click(settingsSubTab);
+	}
+	public void enterMaximumBillAmount(String maxBillAmt) {
+		WebdriverWaits.waitForElementUntilVisible(maxBillAmountTbx, 2);
+		clear_custom(maxBillAmountTbx);
+		pressKeys(maxBillAmountTbx, maxBillAmt);
+		click(maxBillAmountTbx);
+	}
+	public String getMinimumBillAmtValue(){
+		return getAttribute(maxBillAmountTbx,"min");
+	}
+	public String getMaximumBillAmtValue(){
+		return getAttribute(maxBillAmountTbx,"max");
+	}
+	public String getDefaultBillAmtValue(){
+		return getAttribute(maxBillAmountTbx,"value");
+	}
+	public void enableTipGratuityToggleBtn(){
+		click(tipGratuityToggleBtn);
+	}
+	public void clickTipConfigureBtn(){
+		moveToWebElement(tipConfigureBtn);
+		click(tipConfigureBtn);
+	}
+	public String getTipConfigPopUpTitle(){
+		return getText_custom(tipConfgPopUpTitle);
+	}
+	public String getDefaultTipAmtValue(){
+		return getAttribute(tipPercentField1,"value");
+	}
+	public String getMaxTipAmtValue(){
+		return getAttribute(tipPercentField1,"max");
+	}
+	public void enterTipAmt1(String tipAmt1) {
+		WebdriverWaits.waitForElementUntilVisible(tipPercentField1, 2);
+		clear_custom(tipPercentField1);
+		pressKeys(tipPercentField1, tipAmt1);
+		click(tipPercentField1);
+	}
+	public void enterTipAmt2(String tipAmt2) {
+		WebdriverWaits.waitForElementUntilVisible(tipPercentField2, 2);
+		clear_custom(tipPercentField2);
+		pressKeys(tipPercentField2, tipAmt2);
+		click(tipPercentField2);
+	}
+	public void enterTipAmt3(String tipAmt3) {
+		WebdriverWaits.waitForElementUntilVisible(tipPercentField3, 2);
+		clear_custom(tipPercentField3);
+		pressKeys(tipPercentField3, tipAmt3);
+		click(tipPercentField3);
+	}
+	public void clickSaveChangesBtn(){
+		click(saveChangesBtn);
+	}
+	public void clickRewardConfigBtn(){
+		click(rewardConfigureBtn);
+	}
+	public String getRewardConfigPopUpTitle(){
+		return getText_custom(rewardConfigPopUpTitle);
+	}
+	public void enableRewardPointToggleBtn(){
+		click(rewardPointToggleBtn);
+	}
+	public void enterRewardPoints(String rewardPoints) {
+		WebdriverWaits.waitForElementUntilVisible(rewardPointsField, 2);
+		clear_custom(rewardPointsField);
+		pressKeys(rewardPointsField, rewardPoints);
+		click(rewardPointsField);
+	}
+	public String getMinRewardPointsValue(){
+		return getAttribute(rewardPointsField,"min");
+	}
+	public String getMaxRewardPointsValue(){
+		return getAttribute(rewardPointsField,"max");
 	}
 }
--- src/test/java/MyStoreTest.java
+++ src/test/java/MyStoreTest.java
@@ -16,103 +16,231 @@ import org.testng.annotations.Test;
 
 public class MyStoreTest extends BaseTest {
 
-	LoginPage login = new LoginPage();
-	DashBoardPage dashboard = new DashBoardPage();
-	MyStorePage myStore = new MyStorePage();
-//    newBusinessPage newBusiness = new newBusinessPage();
+    LoginPage login = new LoginPage();
+    DashBoardPage dashboard = new DashBoardPage();
+    MyStorePage myStore = new MyStorePage();
 
-	@BeforeMethod
-	public void navigateToDashboard() {
-		login.performSignIn(PropertiesUtil.getPropertyValue("userName"), PropertiesUtil.getPropertyValue("password"));
-	}
+    @BeforeMethod
+    public void navigateToDashboard() {
+        login.performSignIn(PropertiesUtil.getPropertyValue("userName"), PropertiesUtil.getPropertyValue("password"));
+    }
 
-	@AfterMethod
-	public void logout() {
-		dashboard.signOut();
-	}
-	@Test(description="TC_01 Verifying creation of Store without Stripe Payment Account Configuration")
-	public void tc01_StoreCreationWithoutStripeAccount() throws AWTException {
-		//Clicking MyStore sub-tab
-		dashboard.clickOnMyStores();
+    @AfterMethod
+    public void logout() {
+        dashboard.signOut();
+    }
 
-		//Creating New Store
-		myStore.clickRegisterNewBusinessBtn();
-		myStore.clickSkipStripeAccountBtn();
-		WebdriverWaits.sleep(3000);
-		myStore.clickSkipStripeAccountPopUpBtn();
-		WebdriverWaits.sleep(3000);
+    @Test(description = "SC_01 Verifying creation of Store without Stripe Payment Account Configuration")
+    public void sc01_StoreCreationWithoutStripeAccount() throws AWTException {
+        //Clicking MyStore sub-tab
+        dashboard.clickOnMyStores();
 
-		//Entering Store Details
-		myStore.clickStoreLogo();//Fix this
-		myStore.uploadImageAsAttachment("src/main/resources/image/BillDummyImg.jpg");
-		String businessName = "Test Store-1";
-		myStore.enterBusinessName(businessName);
-		String storeAddressName="123";
-		myStore.selectStoreAddress(storeAddressName);
-		String phone="9180652348";
-		myStore.enterPhone(phone);
-		myStore.selectTimeZone();
-		String taxRate="10.000";
-		myStore.enterTax(taxRate);
-		myStore.clickSaveBtn();//Fix this
-	}
+        //Creating New Store
+        myStore.clickRegisterNewBusinessBtn();
+        myStore.clickSkipStripeAccountBtn();
+        WebdriverWaits.sleep(3000);
+        Assertions.assertEquals(myStore.getSkipPopUpTitle(), "Skip");
+        myStore.clickSkipStripeAccountPopUpBtn();
+        WebdriverWaits.sleep(3000);
 
-	@Test(description = "TC_02 Verifying deletion of Store when Stripe Account is not Configured")
-	public void tc02_DeletionOfStore() {
-		//Clicking MyStore sub-tab
-		dashboard.clickOnMyStores();
+        //Entering Store Details
+        myStore.clickSaveBtn();
+        Assertions.assertEquals(myStore.getBlankFieldWarningMsg(), "Please review the highlighted field(s)");
+        myStore.clickStoreLogo();//Fix this
+        myStore.uploadImageAsAttachment("src/main/resources/image/BillDummyImg.jpg");
+        String businessName = "My Store MSC Final";
+        Assertions.assertEquals(myStore.getBusinessFieldMaxLen(), "100");
+        myStore.enterBusinessName(businessName);
+        String locDescription="Without Stripe Account";
+        myStore.enterLocationDescription(locDescription);
+        String storeAddressName = "123";
+        myStore.selectStoreAddress(storeAddressName);
+        String phone = "9180652348";
+        Assertions.assertEquals(myStore.getPhoneFieldMaxLen(), "22");
+        myStore.enterPhone(phone);
+        myStore.selectTimeZone();
+        String taxRate = "10.000";
 
-		myStore.clickRegisterNewBusinessBtn();
+        //Verifying the min, max and default values of taxRate field
+        Assertions.assertEquals(myStore.getTaxFieldMinValue(), "0");
+        Assertions.assertEquals(myStore.getTaxFieldDefaultValue(), "0.000");
+        Assertions.assertEquals(myStore.getTaxFieldMaxValue(), "100");
+        myStore.enterTax(taxRate);
+        myStore.clickSaveBtn();
+        myStore.clickContinueBtn();
+    }
 
-		//Skipping Stripe Account option
-		myStore.clickSkipStripeAccountBtn();
-		WebdriverWaits.sleep(3000);
-		myStore.clickSkipStripeAccountPopUpBtn();
-		WebdriverWaits.sleep(5000);
+    @Test(description = "SC_02 Verifying deletion of Store when Stripe Account is not Configured")
+    public void sc02_DeletionOfStore() {
+        //Clicking MyStore sub-tab
+        dashboard.clickOnMyStores();
+        myStore.clickRegisterNewBusinessBtn();
 
-		//Deleting the Store
-		myStore.clickDeleteStoreBtn();
-		WebdriverWaits.sleep(3000);
-		myStore.clickDeleteStoreIcon();
-	}
-	@Test(description = "TC_03 Verify creation of Store with Stripe Payment Account including terminal configuration")
-	public void tc03_CreationOfStoreWithStripeAccount() {
-		//Clicking MyStore sub-tab
-		dashboard.clickOnMyStores();
+        //Skipping Stripe Account option
+        myStore.clickSkipStripeAccountBtn();
+        WebdriverWaits.sleep(3000);
+        Assertions.assertEquals(myStore.getSkipPopUpTitle(), "Skip");
+        myStore.clickSkipStripeAccountPopUpBtn();
+        WebdriverWaits.sleep(5000);
 
-		myStore.clickRegisterNewBusinessBtn();
-		myStore.clickStripeBtn();
+        //Deleting the Store
+        myStore.clickDeleteStoreBtn();
+        WebdriverWaits.sleep(3000);
+        myStore.clickDeleteStoreIcon();
+    }
 
-		//Creating test Stripe Account
-		myStore.clickTestStripeBtn();
-		myStore.enableBankTransfer();
-		myStore.clickContinueBtn();
-		myStore.clickConfigureCreditCardTerminals();
-		myStore.selectCreditCardTerminal();
-		myStore.clickCreditSaveBtn();
-		myStore.clickSkipForNowBtn();
-		myStore.clickContinueBtn();
-	}
+    @Test(description = "SC_03 Verify creation of Store with Stripe Payment Account")
+    public void sc03_CreationOfStoreWithStripeAccount() {
+        //Click MyStore sub-tab
+        dashboard.clickOnMyStores();
 
-	@Test(description = "TC_04 Verifying modification of existing created Store")
-	public void tc04_VerifyingModificationOfExistingCreatedStore() {
-		//Clicking MyStore sub-tab
-		dashboard.clickOnMyStores();
+        //Click Register New Business Button
+        myStore.clickRegisterNewBusinessBtn();
+        myStore.clickStripeBtn();
 
-		myStore.clickConfigureLink();
-		myStore.clickModifyBtn();
-		String businessName = "Test Store-2";
-		myStore.enterBusinessName(businessName);
-		String storeAddressName="123";
-		myStore.selectStoreAddress(storeAddressName);
-		String phone="9180652341";
-		myStore.enterPhone(phone);
-		myStore.selectTimeZone();
-		String taxRate="18.000";
-		myStore.enterTax(taxRate);
-		myStore.clickSaveBtn();//Fix this
+        //Creating test Stripe Account
+        Assertions.assertEquals(myStore.getConnectStripePopUpTitle(), "Connect to stripe");
+        myStore.clickTestStripeBtn();
+        myStore.enableBankTransfer();
+        myStore.clickContinueBtn();
+        myStore.clickSkipForNowBtn();
+        myStore.clickContinueBtn();
+        WebdriverWaits.sleep(3000);
 
-	}
+        //verifying the default Store Values
+        Assertions.assertEquals(myStore.getAddedStoreName(), "Avenue");
+        Assertions.assertEquals(myStore.getAddedBusinessName(), "Dix Hills");
+        Assertions.assertEquals(myStore.getAddedStoreAddress(), "8 Glover Dr, Dix Hills, NY 11746, USA");
+        Assertions.assertEquals(myStore.getAddedStorePhone(), "+1 (646) 713 6494");
+        Assertions.assertEquals(myStore.getAddedCurrencyOfStore(), "USD");
+        Assertions.assertEquals(myStore.getAddedTaxRate(), "0.000%");
+    }
+
+    @Test(description = "SC_04 Verifying modification of existing created Store")
+    public void sc04_VerifyingModificationOfExistingCreatedStore() {
+        //Click on MyStore sub-tab
+        dashboard.clickOnMyStores();
+
+        //Click on Configure Link
+        myStore.clickConfigureLink();
+
+        //Click on Modify Button
+        myStore.clickModifyBtn();
+        String businessName = "My Store MSC Final";
+        Assertions.assertEquals(myStore.getBusinessFieldMaxLen(), "100");
+        myStore.enterBusinessName(businessName);
+        String locDescription="With Stripe Account";
+        myStore.enterLocationDescription(locDescription);
+        String storeAddressName = "123";
+        myStore.selectStoreAddress(storeAddressName);
+        String phone = "9180652341";
+        Assertions.assertEquals(myStore.getPhoneFieldMaxLen(), "22");
+        myStore.enterPhone(phone);
+        myStore.selectTimeZone();
+        String taxRate = "18.000";
+        Assertions.assertEquals(myStore.getTaxFieldDefaultValue(), "0.000");
+        Assertions.assertEquals(myStore.getTaxFieldMaxValue(), "100");
+        myStore.enterTax(taxRate);
+        myStore.clickSaveBtn();
+        WebdriverWaits.sleep(3000);
+
+        //Verifying Modified Details
+        Assertions.assertEquals(myStore.getAddedStoreName(), businessName);
+        Assertions.assertEquals(myStore.getAddedStorePhone(), "+1 (918) 065 2341");
+        Assertions.assertEquals(myStore.getAddedCurrencyOfStore(), "USD");
+        Assertions.assertEquals(myStore.getAddedTaxRate(), "18.000%");
+    }
+
+    @Test(description = "SC_05 Verifying buying Monthly Business Plan for already created Store")
+    public void sc05_VerifyingBuyingMonthlyBusinessPlanForAlreadyCreatedStore() {
+        //Clicking MyStore sub-tab
+        dashboard.clickOnMyStores();
+
+        //Click on Configure link of already created Store
+        myStore.clickConfigureLink();
+        myStore.clickPlansSubTab();
+
+        //Click on Sign up button
+        Assertions.assertEquals(myStore.getCurrentPlanSuccessMSg(), "Current plan");
+        myStore.clickPlanSignUpBtn();
+        Assertions.assertEquals(myStore.defaultPaymentMethod(), "Visa 1111");
+        myStore.clickChangePayMethodBtn();
+
+        //Verifying that other payment methods are available
+        Assertions.assertTrue(myStore.isNewCreditCardBtnDisplayed());
+        Assertions.assertTrue(myStore.isNewBankAccountBtnDisplayed());
+        myStore.selectVisaMethod();
+        myStore.selectTermsCbx();
+        myStore.clickChangePlanBtn();
+        Assertions.assertTrue(myStore.isNextBillDateDisplayed());//Fix This
+    }
+
+    @Test(description = "SC_06 Verifying buying Yearly Business Plan for already created Store")
+    public void sc06_VerifyingBuyingYearlyBusinessPlanForAlreadyCreatedStore() {
+        //Clicking MyStore sub-tab
+        dashboard.clickOnMyStores();
+
+        //Click on Configure link of already created Store
+        myStore.clickConfigureLink();
+        myStore.clickPlansSubTab();
+        Assertions.assertEquals(myStore.getCurrentPlanSuccessMSg(), "Current plan");
+        myStore.clickYearlyBtn();
+
+        //Click on Sign up button
+        myStore.clickPlanSignUpBtn();
+        Assertions.assertEquals(myStore.defaultPaymentMethod(), "Visa 1111");
+        myStore.clickChangePayMethodBtn();
+
+        //Verifying that other payment methods are available
+        Assertions.assertTrue(myStore.isNewCreditCardBtnDisplayed());
+        Assertions.assertTrue(myStore.isNewBankAccountBtnDisplayed());
+        myStore.selectVisaMethod();//Fix This
+        myStore.selectTermsCbx();
+        myStore.clickChangePlanBtn();
+    }
+
+    @Test(description = "SC_07 Verifying the Configuration of already created Store using Settings Sub-Tabs")
+    public void sc07_VerifyingConfigurationsOfStoreUsingSettings() {
+        //Clicking MyStore sub-tab
+        dashboard.clickOnMyStores();
+
+        //Click on Configure link of already created Store
+        myStore.clickConfigureBtn();
+
+        //Click on Settings Sub-Tab
+        myStore.clickSettingsSubTab();
+
+        //Enter Max Bill Amount
+        String maxBillAmt="5000.00";
+        Assertions.assertEquals(myStore.getMinimumBillAmtValue(),"50.00");
+        Assertions.assertEquals(myStore.getMaximumBillAmtValue(),"50000.00");
+        Assertions.assertEquals(myStore.getDefaultBillAmtValue(),"3000.00");
+        myStore.enterMaximumBillAmount(maxBillAmt);
+        myStore.enableTipGratuityToggleBtn();
+
+        //Configure Tip & Gratuity
+        myStore.clickTipConfigureBtn();
+        Assertions.assertEquals(myStore.getTipConfigPopUpTitle(),"Tip configuration");
+        String tipAmt1="10";
+        String tipAmt2="20";
+        String tipAmt3="30";
+        Assertions.assertEquals(myStore.getDefaultTipAmtValue(),"0");
+        Assertions.assertEquals(myStore.getMaxTipAmtValue(),"99");
+        myStore.enterTipAmt1(tipAmt1);
+        myStore.enterTipAmt2(tipAmt2);
+        myStore.enterTipAmt3(tipAmt3);
+        myStore.clickSaveChangesBtn();
+
+        //Configure Reward Points
+        myStore.clickRewardConfigBtn();
+        Assertions.assertEquals(myStore.getRewardConfigPopUpTitle(),"Rewards Program Configuration");
+        myStore.enableRewardPointToggleBtn();
+        String rewardPoints="1000";
+        Assertions.assertEquals(myStore.getMinRewardPointsValue(), "100");
+        Assertions.assertEquals(myStore.getMaxRewardPointsValue(),"99999");
+        myStore.enterRewardPoints(rewardPoints);
+        myStore.clickSaveChangesBtn();
+    }
 
 }
 
