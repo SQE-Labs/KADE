1da89cc1dc2d5adc395dc107f16c8ae42ff964af
Shruti <shruti.saroha@sqelabs.com>
2024-07-30 18:10:16 +0530

    Modified MyStore Test Cases

--- src/test/java/MyStoreTest.java
+++ src/test/java/MyStoreTest.java
@@ -20,14 +20,14 @@ public class MyStoreTest extends BaseTest {
 	MyStorePage myStore = new MyStorePage();
 
 	//@BeforeMethod
-//	public void navigateToDashboard() {
-//		login.performSignIn(PropertiesUtil.getPropertyValue("userName"), PropertiesUtil.getPropertyValue("password"));
-//	}
-//
-//	//@AfterMethod
-//	public void logout() {
-//		dashboard.signOut();
-//	}
+	public void navigateToDashboard() {
+		login.performSignIn(PropertiesUtil.getPropertyValue("userName"), PropertiesUtil.getPropertyValue("password"));
+	}
+
+	//@AfterMethod
+	public void logout() {
+		dashboard.signOut();
+	}
 
 	@Test(description = "SC_01 Verifying creation of Store without Stripe Payment Account Configuration")
 	public void sc01_StoreCreationWithoutStripeAccount() throws AWTException {
@@ -37,17 +37,18 @@ public class MyStoreTest extends BaseTest {
 
 		//Creating New Store
 		myStore.getRegisterNewBusinessButton().click();
-		myStore.getSkipStripeAccountButton().clickIfExist();
-//		WebdriverWaits.sleep(3000);
-//		Assertions.assertEquals(myStore.getSkipPopUpTitle(), "Skip");
-		myStore.getSkipStripeAccountPopUpButton().clickIfExist();
-//		WebdriverWaits.sleep(3000);
+		myStore.getSkipStripeAccountButton().click();//clickIfExist doesn't work
+		WebdriverWaits.sleep(3000);
+		Assertions.assertEquals(myStore.getSkipPopUpTitle(), "Skip");
+		myStore.getSkipStripeAccountPopUpButton().click();
+		WebdriverWaits.sleep(3000);
 
 		//Entering Store Details
-		myStore.getSaveButton().click();
+		myStore.getSaveButton().clickbyJS();
 		Assertions.assertEquals(myStore.getBlankFieldWarningMsg(), "Please review the highlighted field(s)");
-		myStore.getStoreLogo().click();//Fix this
+		myStore.getStoreLogo().click();
 		myStore.uploadImageAsAttachment("src/main/resources/image/BillDummyImg.jpg");
+		myStore.getCheckButton().clickbyJS();
 		Assertions.assertEquals(myStore.getBusinessFieldMaxLen(), "100");
 		myStore.getBusinessNameField().setText("My Store Final");
 		myStore.getLocationDescriptionField().setText("Without Stripe Account");
@@ -62,8 +63,8 @@ public class MyStoreTest extends BaseTest {
 		Assertions.assertEquals(myStore.getTaxFieldDefaultValue(), "0.000");
 		Assertions.assertEquals(myStore.getTaxFieldMaxValue(), "100");
 		myStore.getTaxRateField().setText("10.000");
-		myStore.getSaveButton().click();
-		myStore.getContinueButton().click();
+		myStore.getSaveButton().clickbyJS();
+		myStore.getContinueButton().clickbyJS();
 	}
 
 	@Test(description = "SC_02 Verifying deletion of Store when Stripe Account is not Configured")
@@ -82,9 +83,9 @@ public class MyStoreTest extends BaseTest {
 		WebdriverWaits.sleep(5000);
 
 		//Deleting the Store
-		myStore.getDeleteStoreButton().click();
+		myStore.getDeleteStoreButton().clickbyJS();
 		WebdriverWaits.sleep(3000);
-		myStore.getDeleteStoreIcon().click();
+		myStore.getDeleteStoreIcon().clickbyJS();
 	}
 
 	@Test(description = "SC_03 Verify creation of Store with Stripe Payment Account")
@@ -100,9 +101,9 @@ public class MyStoreTest extends BaseTest {
 		Assertions.assertEquals(myStore.getConnectStripePopUpTitle(), "Connect to stripe");
 		myStore.getTestStripeAccountButton().click();
 		myStore.getBankTransferToggleButton().click();
-		myStore.getContinueButton().click();
+		myStore.getContinueButton().clickbyJS();
 		myStore.getSkipForNowButton().click();
-		myStore.getContinueButton().click();
+		myStore.getContinueButton().clickbyJS();
 		WebdriverWaits.sleep(3000);
 
 		//verifying the default Store Values
@@ -115,17 +116,19 @@ public class MyStoreTest extends BaseTest {
 	}
 
 	@Test(description = "SC_04 Verifying modification of existing created Store")
-	public void sc04_VerifyingModificationOfExistingCreatedStore() {
+	public void sc04_VerifyingModificationOfExistingCreatedStore() throws AWTException {
 		KadeSession session = KadeSession.login(KadeUserAccount.Default);
 		session.getDashBoardPage().getMyStoresTab().click();
 		MyStorePage myStore= session.getMyStorePage();
-
 
 		//Click on Configure Link
 		myStore.getConfigureLink().click();
 
 		//Click on Modify Button
 		myStore.getModifyButton().click();
+		myStore.getStoreLogo().click();
+		myStore.uploadImageAsAttachment("src/main/resources/image/BillDummyImg.jpg");
+		myStore.getCheckButton().clickbyJS();
 		Assertions.assertEquals(myStore.getBusinessFieldMaxLen(), "100");
 		myStore.getBusinessNameField().setText("My Store MSC Final");
 		myStore.getLocationDescriptionField().setText("With Stripe Account");
@@ -137,7 +140,7 @@ public class MyStoreTest extends BaseTest {
 		Assertions.assertEquals(myStore.getTaxFieldDefaultValue(), "0.000");
 		Assertions.assertEquals(myStore.getTaxFieldMaxValue(), "100");
 		myStore.getTaxRateField().setText("18.000");
-		myStore.getSaveButton().click();
+		myStore.getSaveButton().clickbyJS();
 		WebdriverWaits.sleep(3000);
 
 		//Verifying Modified Details
@@ -160,82 +163,81 @@ public class MyStoreTest extends BaseTest {
 		Assertions.assertEquals(myStore.getCurrentPlanSuccessMSg(), "Current plan");
 		myStore.getPlansSignUpButton().click();
 		Assertions.assertEquals(myStore.defaultPaymentMethod(), "Visa 1111");
-//		myStore.getChangePlanButton().clickChangePayMethodBtn();
+		myStore.getChangePayMethodLink().click();
 
 		//Verifying that other payment methods are available
 		Assertions.assertTrue(myStore.isNewCreditCardBtnDisplayed());
 		Assertions.assertTrue(myStore.isNewBankAccountBtnDisplayed());
-		myStore.selectVisaMethod();
-		myStore.selectTermsCbx();
-		myStore.clickChangePlanBtn();
+		myStore.getTermsCheckbox().click();
+		myStore.getChangePlanButton().click();
 		Assertions.assertTrue(myStore.isNextBillDateDisplayed());//Fix This
 	}
 
 	@Test(description = "SC_06 Verifying buying Yearly Business Plan for already created Store")
 	public void sc06_VerifyingBuyingYearlyBusinessPlanForAlreadyCreatedStore() {
-		//Clicking MyStore sub-tab
-		dashboard.clickOnMyStores();
+		KadeSession session = KadeSession.login(KadeUserAccount.Default);
+		session.getDashBoardPage().getMyStoresTab().click();
+		MyStorePage myStore= session.getMyStorePage();
 
-		//Click on Configure link of already created Store
-		myStore.clickConfigureLink();
-		myStore.clickPlansSubTab();
+		myStore.getConfigureLink().click();
+		myStore.getPlansSubTab().click();
 		Assertions.assertEquals(myStore.getCurrentPlanSuccessMSg(), "Current plan");
-		myStore.clickYearlyBtn();
+		myStore.getYearlyPlanButton().click();
 
 		//Click on Sign up button
-		myStore.clickPlanSignUpBtn();
+		myStore.getPlansSignUpButton().click();
 		Assertions.assertEquals(myStore.defaultPaymentMethod(), "Visa 1111");
-		myStore.clickChangePayMethodBtn();
+		myStore.getChangePayMethodLink().click();
 
 		//Verifying that other payment methods are available
 		Assertions.assertTrue(myStore.isNewCreditCardBtnDisplayed());
 		Assertions.assertTrue(myStore.isNewBankAccountBtnDisplayed());
-		myStore.selectVisaMethod();//Fix This
-		myStore.selectTermsCbx();
-		myStore.clickChangePlanBtn();
+		myStore.getTermsCheckbox().click();
+		myStore.getChangePlanButton().click();
 	}
 
 	@Test(description = "SC_07 Verifying the Configuration of already created Store using Settings Sub-Tabs")
 	public void sc07_VerifyingConfigurationsOfStoreUsingSettings() {
-		//Clicking MyStore sub-tab
-		dashboard.clickOnMyStores();
+		KadeSession session = KadeSession.login(KadeUserAccount.Default);
+		session.getDashBoardPage().getMyStoresTab().click();
+		MyStorePage myStore= session.getMyStorePage();
 
-		//Click on Configure link of already created Store
-		myStore.clickConfigureBtn();
+		myStore.getConfigureButton().click();
 
 		//Click on Settings Sub-Tab
-		myStore.clickSettingsSubTab();
+		myStore.getSettingsSubTab().click();
 
 		//Enter Max Bill Amount
-		String maxBillAmt="5000.00";
 		Assertions.assertEquals(myStore.getMinimumBillAmtValue(),"50.00");
 		Assertions.assertEquals(myStore.getMaximumBillAmtValue(),"50000.00");
 		Assertions.assertEquals(myStore.getDefaultBillAmtValue(),"3000.00");
-		myStore.enterMaximumBillAmount(maxBillAmt);
-		myStore.enableTipGratuityToggleBtn();
+		myStore.getMaximumBillAmountField().setText("5000.00");
+		myStore.getTipGrauityToggleButton().click();
 
 		//Configure Tip & Gratuity
-		myStore.clickTipConfigureBtn();
+		myStore.getTipConfigureButton().click();
 		Assertions.assertEquals(myStore.getTipConfigPopUpTitle(),"Tip configuration");
-		String tipAmt1="10";
-		String tipAmt2="20";
-		String tipAmt3="30";
 		Assertions.assertEquals(myStore.getDefaultTipAmtValue(),"0");
 		Assertions.assertEquals(myStore.getMaxTipAmtValue(),"99");
-		myStore.enterTipAmt1(tipAmt1);
-		myStore.enterTipAmt2(tipAmt2);
-		myStore.enterTipAmt3(tipAmt3);
-		myStore.clickSaveChangesBtn();
+		myStore.getTipAmountField1().setText("10");
+		myStore.getTipAmountField2().setText("20");
+		myStore.getTipAmountField3().setText("30");
+		myStore.getSaveChangesButton().click();
 
 		//Configure Reward Points
-		myStore.clickRewardConfigBtn();
+		myStore.getRewardConfigureButton().clickbyJS();
 		Assertions.assertEquals(myStore.getRewardConfigPopUpTitle(),"Rewards Program Configuration");
-		myStore.enableRewardPointToggleBtn();
-		String rewardPoints="1000";
+		myStore.getRewardPointToggleButton().click();
 		Assertions.assertEquals(myStore.getMinRewardPointsValue(), "100");
 		Assertions.assertEquals(myStore.getMaxRewardPointsValue(),"99999");
-		myStore.enterRewardPoints(rewardPoints);
-		myStore.clickSaveChangesBtn();
+		myStore.getRewardPointsField().setText("1000");
+		myStore.getSaveChangesButton().click();
+
+		//Configure Store Links
+//		myStore.getStoreLinksButton().click();
+//		Assertions.assertEquals(myStore.getMinRewardPointsFieldValue(), "1");
+//		Assertions.assertEquals(myStore.getMaxRewardPointsFieldValue(),"9999");
+//		myStore.getRewardPointsValueField().setText("1000");
 	}
 
 }
