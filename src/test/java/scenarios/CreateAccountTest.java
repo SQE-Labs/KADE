f0670ce17e6cb76e023901d880a78f085b111e0d
Pankaj Hembram <pankaj.hembram@sqelabs.com>
2024-09-10 16:50:32 +0530

    Automated Create new account test case

--- src/test/java/scenarios/CreateAccountTest.java
+++ src/test/java/scenarios/CreateAccountTest.java
@@ -5,9 +5,10 @@ import org.automation.session.KadeSession;
 import org.automation.utilities.Assertions;
 import org.automation.utilities.RandomGenerator;
 import org.automation.utilities.WebdriverWaits;
+import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
-public class CreateAccountTest extends BaseTest  {
+public class CreateAccountTest extends BaseTest {
 
     KadeSession session = new KadeSession();
     RandomGenerator randomGenerator = new RandomGenerator();
@@ -48,8 +49,9 @@ public class CreateAccountTest extends BaseTest  {
         session.getCreateAccountPage().getSignInLink().click();
 
     }
-    @Test (description = "CA_TC 1(b): Verify the validation messages while creating new account with 'Personal Account' option on 'Login' page.")
-    public void verifyValidationMessagesWithPersonalAccountOption(){
+
+    @Test(description = "CA_TC 1(b): Verify the validation messages while creating new account with 'Personal Account' option on 'Login' page.")
+    public void verifyValidationMessagesWithPersonalAccountOption() {
         session.getLoginPage().getSignUpLink().click();
 
         // Clicking on Personal Account Option
@@ -59,7 +61,7 @@ public class CreateAccountTest extends BaseTest  {
         // Verify the Please review the highlighted field validation message and This field is required Tooltip
         Assertions.assertTrue(session.getLoginPage().getValidationMessage().isDisplayed());
         String actualMessage = session.getCreateAccountPage().getEmailOrPhoneField().getToolTipMessage();
-        Assertions.assertEquals(actualMessage,"This field is required.");
+        Assertions.assertEquals(actualMessage, "This field is required.");
 
         // Enter invalid data in 'Email or Phone' field
         session.getCreateAccountPage().getEmailOrPhoneField().setText("invalid123");
@@ -68,16 +70,17 @@ public class CreateAccountTest extends BaseTest  {
 
         // tooltip message
         String actualToolTip = session.getCreateAccountPage().getEmailOrPhoneField().getToolTipMessage();
-        Assertions.assertEquals(actualToolTip,"Invalid phone or email. Phone numbers must start with country code starting with +");
+        Assertions.assertEquals(actualToolTip, "Invalid phone or email. Phone numbers must start with country code starting with +");
 
         // Enter invalid email
-        session.getCreateAccountPage().getEmailOrPhoneField().setText("test@123");
-        session.getCreateAccountPage().getSignUpButton().click();
-        String invalidEmailTooltip= session.getCreateAccountPage().getEmailOrPhoneField().getToolTipMessage();
-        Assertions.assertEquals(invalidEmailTooltip,"Please enter a valid email address.");
+        session.getCreateAccountPage().getEmailOrPhoneField().setText("test123@d");
+        session.getCreateAccountPage().getSignUpButton().clickByMouse();
+
+        String invalidEmailTooltip = session.getCreateAccountPage().getEmailOrPhoneField().getToolTipMessage();
+        Assertions.assertEquals(invalidEmailTooltip, "Please enter a valid email address.");
         //session.getCreateAccountPage().getCrossICon().click();
 
-         // Entering existing phone number and verify the validation message
+        // Entering existing phone number and verify the validation message
         session.getCreateAccountPage().getEmailOrPhoneField().setText("6465551114");
         session.getCreateAccountPage().getSignUpButton().click();
 
@@ -95,14 +98,14 @@ public class CreateAccountTest extends BaseTest  {
                 ExpectedEmailMessage);
     }
 
-    @Test (description = "CA_TC 1(c): Verify that creating new account with 'Personal Account' option.")
+    @Test(description = "CA_TC 1(c): Verify that creating new account with 'Personal Account' option.")
     public void verifyCreatingAccountWithPersonalAccountOption() {
         session.getLoginPage().getSignUpLink().click();
 
         // Clicking on Personal Account Option
         session.getCreateAccountPage().getPersonalAccountButton().click();
         String st = randomGenerator.requiredString(6);
-        session.getCreateAccountPage().getEmailOrPhoneField().setText(st+"@yopmail.com");
+        session.getCreateAccountPage().getEmailOrPhoneField().setText(st + "@yopmail.com");
         session.getCreateAccountPage().getReceiveTextEmailNotificationCheckBox().click();
         session.getCreateAccountPage().getSignUpButton().click();
 
@@ -121,16 +124,16 @@ public class CreateAccountTest extends BaseTest  {
         session.getCreateAccountPage().getSubmitButton().click();
 
         // Verify the tooltip message
-       String tooltipMessage= (session.getCreateAccountPage().getPasswordField().getToolTipMessage());
-       Assertions.assertEquals(tooltipMessage,"This field is required.");
+        String tooltipMessage = (session.getCreateAccountPage().getPasswordField().getToolTipMessage());
+        Assertions.assertEquals(tooltipMessage, "This field is required.");
 
-       // Enter invalid data in Password Field
+        // Enter invalid data in Password Field
         session.getCreateAccountPage().getPasswordField().setText("abcdefgh");
         session.getCreateAccountPage().getSubmitButton().click();
 
         // Verify tooltip message in password field
-        String ActualTooltip= session.getCreateAccountPage().getPasswordField().getToolTipMessage();
-        Assertions.assertEquals(ActualTooltip,"Invalid password, a password must contain at least one upper case letter, one lower case letter and one number.");
+        String ActualTooltip = session.getCreateAccountPage().getPasswordField().getToolTipMessage();
+        Assertions.assertEquals(ActualTooltip, "Invalid password, a password must contain at least one upper case letter, one lower case letter and one number.");
 
         // Enter Password and Confirm Password
         session.getCreateAccountPage().getPasswordField().setText("Text@123");
@@ -142,18 +145,18 @@ public class CreateAccountTest extends BaseTest  {
     }
 
     @Test(description = "CA_TC 2(a): Verify that creating a new account by email address with  Business Account option.")
-        public void verifyCreatingNewAccountByEmailWithBusinessAccount(){
+    public void verifyCreatingNewAccountByEmailWithBusinessAccount() {
         session.getLoginPage().getSignUpLink().click();
 
-          //Clicking on Business Account Option
+        //Clicking on Business Account Option
         session.getCreateAccountPage().getBusinessAccountButton().click();
 
         // Verify Mobile field label
         Assertions.assertTrue(session.getCreateAccountPage().getMobilePhoneFieldLabel().isDisplayed());
 
         // Verify the  Use Email Link
-         Assertions.assertTrue(session.getCreateAccountPage().getUseEmailLink().isDisplayed());
-         // Verify the Receive Text Email Notification CheckBox
+        Assertions.assertTrue(session.getCreateAccountPage().getUseEmailLink().isDisplayed());
+        // Verify the Receive Text Email Notification CheckBox
         Assertions.assertTrue(session.getCreateAccountPage().getReceiveTextEmailNotificationCheckBox().isDisplayed());
         // Verify the Continue Button
         Assertions.assertTrue(session.getCreateAccountPage().getContinueButton().isDisplayed());
@@ -172,12 +175,12 @@ public class CreateAccountTest extends BaseTest  {
         session.getCreateAccountPage().getContinueButton().click();
 
         // Verify the email tooltip
-        String Actual= session.getCreateAccountPage().getEmailBusinessAccountField().getToolTipMessage();
-        Assertions.assertEquals(Actual,"Please enter a valid email address.");
+        String Actual = session.getCreateAccountPage().getEmailBusinessAccountField().getToolTipMessage();
+        Assertions.assertEquals(Actual, "Please enter a valid email address.");
 
         // Entering valid email in the Email field
-         String st = randomGenerator.requiredString(3);
-        session.getCreateAccountPage().getEmailBusinessAccountField().setText(st+"@yopmail.com");
+        String st = randomGenerator.requiredString(3);
+        session.getCreateAccountPage().getEmailBusinessAccountField().setText(st + "@yopmail.com");
 
         // Checking the By providing my information, I consent to receive text/email notifications. checkbox
         session.getCreateAccountPage().getReceiveTextEmailNotificationCheckBox().click();
@@ -200,14 +203,14 @@ public class CreateAccountTest extends BaseTest  {
         session.getCreateAccountPage().getSecurityCodeField().setText("123456");
 
         //Entering name in the Full name field
-        session.getCreateAccountPage().getFullName().setText("New User "+  st);
+        session.getCreateAccountPage().getFullName().setText("New User " + st);
 
         // Clicking on 'Continue' button
         session.getCreateAccountPage().getContinueButtonOfBusinessAccount().click();
     }
 
     @Test(description = "CA_TC 2(b) : Verify that creating new account by phone number with Business Account option.")
-    public void verifyCreateNewAccountByPhoneNumberWithBusinessAccount(){
+    public void verifyCreateNewAccountByPhoneNumberWithBusinessAccount() {
         String Phone = "6465551119";
         session.getLoginPage().getSignUpLink().click();
 
@@ -248,8 +251,8 @@ public class CreateAccountTest extends BaseTest  {
         session.getCreateAccountPage().getContinueButtonOfBusinessAccount().click();
 
         // Verify the Tooltip message for security field
-        String TooltipForLessDigit= session.getCreateAccountPage().getSecurityCodeField().getToolTipMessage();
-        Assertions.assertEquals(TooltipForLessDigit,"Please enter at least 6 characters.");
+        String TooltipForLessDigit = session.getCreateAccountPage().getSecurityCodeField().getToolTipMessage();
+        Assertions.assertEquals(TooltipForLessDigit, "Please enter at least 6 characters.");
 
         // Entering Invalid security Code
         session.getCreateAccountPage().getSecurityCodeField().setText("123458");
@@ -257,7 +260,7 @@ public class CreateAccountTest extends BaseTest  {
         session.getCreateAccountPage().getContinueButtonOfBusinessAccount().click();
 
         // Verify the Validation message for Incorrect Security Code
-        Assertions.assertEquals(session.getCreateAccountPage().getSecurityCodeAlertMessage().getText(),"Invalid security code");
+        Assertions.assertEquals(session.getCreateAccountPage().getSecurityCodeAlertMessage().getText(), "Invalid security code");
 
         // Entering Valid security Code
         session.getCreateAccountPage().getSecurityCodeField().setText("777777");
@@ -277,20 +280,67 @@ public class CreateAccountTest extends BaseTest  {
         // Click on Close and Delete Account button
         session.getSecurityAndPasswordPage().getDeleteAccountButton().click();
         session.getSecurityAndPasswordPage().getTickIcon().click();
-
-
-
-
-
-
-
-
     }
 
+    @Test(description = "CA_TC 3: Verify that signing in to the application by phone number using Business Account option.")
+    public void verifyThatSiginToApplicationByPhoneNumberUsingBusinessAccountOption() {
+        session.getLoginPage().getSignUpLink().click();
+        session.getCreateAccountPage().getBusinessAccountButton().click();
+        session.getCreateAccountPage().getCrossICon().click();
+
+        // Clicking on 'Sign In Button
+        session.getCreateAccountPage().getSignInButton().click();
+
+        // Verify the elements on 'Sign In' page
+        Assertions.assertTrue(session.getCreateAccountPage().getSignInPopupTitle().isDisplayed());
+        Assertions.assertTrue(session.getCreateAccountPage().getSignInContinue().isDisplayed());
+        Assertions.assertTrue(session.getCreateAccountPage().getSwitchToEmailLink().isDisplayed());
+        Assertions.assertTrue(session.getCreateAccountPage().getPhoneFieldOfSignIn().isDisplayed());
+        Assertions.assertTrue(session.getCreateAccountPage().getPhoneLabelofSignIn().isDisplayed());
+        Assertions.assertTrue(session.getCreateAccountPage().getSignInWithGoogleImage().isDisplayed());
+        Assertions.assertTrue(session.getCreateAccountPage().getSignInWithAppleImage().isDisplayed());
+
+        // Validation for Phone Field
+        session.getCreateAccountPage().getSignInContinue().click();
+        String Actual = session.getCreateAccountPage().getPhoneFieldOfSignIn().getToolTipMessage();
+        Assertions.assertEquals(Actual, "This field is required.");
+        session.getCreateAccountPage().getSignInContinue().click();
+        session.getCreateAccountPage().getPhoneFieldOfSignIn().setText("12345");
+        session.getCreateAccountPage().getSignInContinue().clickByMouse();
+        Assertions.assertTrue(session.getCreateAccountPage().getPleaseReviewValidation().isDisplayed());
+        String ActualTooltip = session.getCreateAccountPage().getPhoneFieldOfSignIn().getToolTipMessage();
+        Assertions.assertEquals(ActualTooltip, "Invalid phone number");
+
+        // Clearing Phone Field
+        session.getCreateAccountPage().getPhoneFieldOfSignIn().clearMaskedInputField(By.xpath("//input[@name='userName' and @data-f-type='phone']"));
+        session.getCreateAccountPage().getPhoneField().click();
+
+        // Entering valid phone number
+        session.getCreateAccountPage().getPhoneFieldOfSignIn().setText("6465551114");
+        session.getCreateAccountPage().getSignInContinue().click();
+
+        // Verify the link is displayed
+        Assertions.assertTrue(session.getCreateAccountPage().getChangeLink().isDisplayed());
+        Assertions.assertTrue(session.getCreateAccountPage().getIdontKnowPasswordLink().isDisplayed());
+        Assertions.assertTrue(session.getCreateAccountPage().getFinalSignInButton().isDisplayed());
+
+        // Validation for Password field
+        session.getCreateAccountPage().getFinalSignInButton().click();
+        session.getCreateAccountPage().getSignInPasswordField().getToolTipMessage();
+        session.getCreateAccountPage().getSignInPasswordField().setText("33432533");
+        session.getCreateAccountPage().getFinalSignInButton().click();
+        WebdriverWaits.sleep(2000);
+        String ActualAlertMessage = session.getCreateAccountPage().getSignInPasswordField().getToolTipMessage();
+        Assertions.assertEquals(ActualAlertMessage,
+                "Invalid password, a password must contain at least one upper case letter, one lower case letter and one special character or a number.");
+
+        // Entering valid password and signning in to the app.
+        session.getCreateAccountPage().getFinalShowPassword().click();
+        session.getCreateAccountPage().getSignInPasswordField().setText("Test@123");
+        session.getCreateAccountPage().getFinalSignInButton().click();
+    }
 
 }
-
-
 
 
 
