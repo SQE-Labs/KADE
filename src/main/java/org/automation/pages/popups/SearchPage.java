2ac1e39403412679d2a14bd33dc6a74a7f2c8c7c
Automation-Intern <sarishti@sqelabs.com>
2025-01-13 18:47:03 +0530

    Automated 2 TestCases of Search Module

--- src/main/java/org/automation/pages/popups/SearchPage.java
+++ src/main/java/org/automation/pages/popups/SearchPage.java
@@ -1,14 +1,36 @@
 package org.automation.pages.popups;
 
+import org.apache.poi.util.NullLogger;
 import org.automation.ReturnObjects.Clickable;
+import org.automation.ReturnObjects.Editable;
 import org.automation.base.BasePage;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.awt.*;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.UnsupportedFlavorException;
+import java.io.IOException;
+import java.util.List;
+import java.util.Random;
 
 public class SearchPage extends BasePage {
     //Locators
     By SearchRow = By.xpath("//a[@class = 'sidebar-link']");
     By SearchPopup = By.cssSelector(".modal-content");
-
+    By SearchLabel = By.xpath("//div[@class = 'modal-body']/div");
+    By SearchClose = By.xpath("//div[@class = 'modal-header']/button");
+    By SearchBillReferenceId = By.xpath("(//div[@class='col-7    ']/span)[1]");
+    public By CopyReferenceId = By.xpath("//div[@class='mb-1']/span");
+    By CloseBillButton = By.xpath("//div[@class='modal-header']/button");
+    public By ClickSearchBox = By.xpath("//input[@name='phrase']");
+    By ClickSearchIcon =By.xpath("//div[@class='input-group']/button");
+    By Billid = By.xpath("//div[@class='mb-1']/span");
+    //By BillLabel = By.xpath("//span[text() = 'Bill']");
+    public By SearchTransReferenceId = By.xpath("(//div[@class='position-relative loaded']/div/div/span)[1]");
+    By BillPageReferenceId = By.xpath("(//div[@class='modal-body']/div/div)[3]");
+    public By TransactionId = By.xpath("(//div[@class='col-7']/div/span)[1]");
 
 
     public SearchPage()
@@ -17,8 +39,52 @@ public class SearchPage extends BasePage {
     }
 
     public Clickable SearchPopup() {
-        return Clickable.getElementBy(SearchPopup,"Find or Add Customer");
+        return Clickable.getElementBy(SearchPopup,"Search Popup");
     }
+
+    public Clickable closeSearchPopup() {
+        return Clickable.getElementBy(SearchClose,"Search Popup Close");
+    }
+
+    public Editable getSearchLabel() {
+        return Editable.getElementBy(SearchLabel, "Search Label");
+    }
+
+    public Clickable getSearchBillReferenceId() {
+        return Clickable.getElementBy(SearchBillReferenceId, "Search Reference Id");
+    }
+    public Clickable getCopyReferenceId() {
+        return Clickable.getElementBy(CopyReferenceId, "Copy Reference Id");
+    }
+    public Clickable getCloseBillButton() {
+        return Clickable.getElementBy(CloseBillButton, "Close Bill Button");
+    }
+    public Clickable getClickSearchBox() {
+
+        return Clickable.getElementBy(ClickSearchBox, "Click Search Box");
+    }
+    public String getClipboardText() throws IOException, UnsupportedFlavorException {
+        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+        String clipboardtext = (String) clipboard.getData(DataFlavor.stringFlavor);
+        return clipboardtext;
+    }
+    public Clickable getClickSearchIcon() {
+        return Clickable.getElementBy(ClickSearchIcon,"Click Search Icon");
+    }
+    public Clickable getBillid() {
+        return Clickable.getElementBy(Billid,"Bill Label");
+    }
+    public Clickable getSearchTransReferenceId() {
+        return Clickable.getElementBy(SearchTransReferenceId,"Search Transaction Reference Id");
+    }
+    public Clickable getBillPageReferenceId() {
+        return Clickable.getElementBy(BillPageReferenceId,"Bill Page Reference Id");
+    }
+    public Clickable getTransactionId() {
+        return Clickable.getElementBy(TransactionId,"Transaction Id");
+    }
+
+
 
 
 
--- src/test/java/scenarios/SearchTest.java
+++ src/test/java/scenarios/SearchTest.java
@@ -2,20 +2,83 @@ package scenarios;
 
 import org.automation.base.BaseTest;
 import org.automation.data.KadeUserAccount;
-import org.automation.pages.BillPage;
-import org.automation.pages.SidePannel;
-import org.automation.pages.popups.SearchPage;
 import org.automation.session.KadeSession;
 import org.automation.utilities.Assertions;
+import org.automation.utilities.WebdriverWaits;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
+
+import java.awt.datatransfer.UnsupportedFlavorException;
+import java.io.IOException;
+
 
 public class SearchTest extends BaseTest {
     @Test(description = "Verify that 'Search' popup appears after clicking on 'Search' tab, on Left Panel.")
-    public void verifySearchPopupAppearsAfterClickingSearchTab()
-    {
+    public void verifySearchPopupAppearsAfterClickingSearchTab() throws IOException, UnsupportedFlavorException {
         KadeSession session = KadeSession.login(KadeUserAccount.SearchUser);
+        //TestCase 1
+        //Click on Search Tab
         session.getSidePannel().getSearchTab().click();
+        //Verify Search Popup appears
         Assertions.assertTrue(session.getSearchPage().SearchPopup().isDisplayed());
+
+        //TestCase 2
+        //Verify Search Label
+        String actualSearchLabel = session.getSearchPage().getSearchLabel().getText();
+        String expectedSearchLabel = "Search by reference Id, phone or email";
+        Assertions.assertEquals(expectedSearchLabel, actualSearchLabel);
+
+    }
+    @Test(description = "Verify that 'bill or transaction' popup appears after searching with a reference ID in 'Search by Reference ID, Phone, or Email' field.")
+    public void verifyBillOrTransactionAppearsAfterSearchingByReferenceId() throws IOException, UnsupportedFlavorException {
+
+        KadeSession session = KadeSession.login(KadeUserAccount.SearchUser);
+        //Test case 3
+        //Open Bill Page
+        session.getSidePannel().getBillButton().click();
+        //Click On ReferenceId
+        session.getSearchPage().getSearchBillReferenceId().clickByMouse();
+        WebdriverWaits.waitForElementUntilVisible(session.getSearchPage().CopyReferenceId,5);
+        //Copy ReferenceId
+        session.getSearchPage().getCopyReferenceId().clickByMouse();
+        //Close Bill Popup
+        session.getSearchPage().getCloseBillButton().click();
+        //Click on Search Tab
+        session.getSidePannel().getSearchTab().click();
+        //Click on Search Box
+        session.getSearchPage().getClickSearchBox().click();
+        //Get copied text from clipboard
+        String copiedText = session.getSearchPage().getClipboardText();
+        WebElement Searchbox = session.getSearchPage().getClickSearchBox().getElement(session.getSearchPage().ClickSearchBox);
+        //Paste Copied Text
+        Searchbox.sendKeys(copiedText);
+        //Click on Search Icon
+        session.getSearchPage().getClickSearchIcon().click();
+        //Get BillId from popup
+        session.getSearchPage().getBillid().click();
+        String CopiedBillId = session.getSearchPage().getClipboardText();
+        Assertions.assertEquals(copiedText, CopiedBillId);
+        //Close Bill Popup
+        session.getSearchPage().getCloseBillButton().click();
+        //Click on Transaction Tab
+        session.getSidePannel().getTransactionButton().click();
+        //Click On ReferenceId
+        System.out.println(session.getSearchPage().getSearchTransReferenceId().click());
+        //Click on Search Tab
+        session.getSidePannel().getSearchTab().click();
+        //Click on Search Box
+        session.getSearchPage().getClickSearchBox().click();
+        //Get copied text from clipboard
+        String copiedTransId = session.getSearchPage().getClipboardText();
+        WebElement Searchbox2 = session.getSearchPage().getClickSearchBox().getElement(session.getSearchPage().ClickSearchBox);
+        //Paste Copied Text
+        Searchbox2.sendKeys(copiedTransId);
+        //Click on Search Icon
+        session.getSearchPage().getClickSearchIcon().click();
+        //Get Transaction Id
+        session.getSearchPage().getTransactionId().click();
+        String CopyActualTransactionId = session.getSearchPage().getClipboardText();
+        Assertions.assertEquals(CopyActualTransactionId,copiedTransId);
 
     }
 }
