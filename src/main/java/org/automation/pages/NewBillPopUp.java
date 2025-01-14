03d6a6ced5c662fb63c97bf3a6e64acc015e6be9
Preeti1484 <Preeti@sqelabs.com>
2025-01-14 11:58:32 +0530

    Adding new changes

--- src/main/java/org/automation/pages/NewBillPopUp.java
+++
@@ -1,6 +0,0 @@
-package org.automation.pages;
-import org.automation.base.BasePage;
-
-public class NewBillPopUp extends BasePage {
-
-}
--- src/main/java/org/automation/pages/ReportsPage.java
+++
@@ -1,8 +0,0 @@
-package org.automation.pages;
-
-import org.automation.base.BasePage;
-
-public class ReportsPage extends BasePage {
-	
-	
-}
